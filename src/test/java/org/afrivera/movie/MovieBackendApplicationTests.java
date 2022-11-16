054e115308857440835ae1849787f936b6824397
afrivera <andresrfelip@hotmail.com>
2022-11-15 22:42:37 -0500

    first commit

---
+++ src/test/java/org/afrivera/movie/MovieBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afrivera.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
