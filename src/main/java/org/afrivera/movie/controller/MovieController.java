b71f93533a6c5c6231718020b8d4398fb689b401
afrivera <andresrfelip@hotmail.com>
2022-11-18 16:43:18 -0500

    add exception RosourceNotfoundException to movies not found

--- src/main/java/org/afrivera/movie/controller/MovieController.java
+++ src/main/java/org/afrivera/movie/controller/MovieController.java
@@ -3,6 +3,7 @@ package org.afrivera.movie.controller;
 import lombok.RequiredArgsConstructor;
 import org.afrivera.movie.dto.MovieDto;
 import org.afrivera.movie.service.MovieService;
+import org.afrivera.movie.service.impl.MovieServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieService movieService;
+    private final MovieServiceImpl movieService;
 
     @GetMapping
     public ResponseEntity<List<MovieDto>> getAllMovies(){
--- src/main/java/org/afrivera/movie/service/impl/MovieServiceImpl.java
+++ src/main/java/org/afrivera/movie/service/impl/MovieServiceImpl.java
@@ -3,6 +3,7 @@ package org.afrivera.movie.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.afrivera.movie.dto.MovieDto;
 import org.afrivera.movie.entity.Movie;
+import org.afrivera.movie.exception.ResourceNotFoundException;
 import org.afrivera.movie.mapper.MovieMapper;
 import org.afrivera.movie.repository.MovieRepository;
 import org.afrivera.movie.service.MovieService;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,16 +32,14 @@ public class MovieServiceImpl implements MovieService {
     @Override
     @Transactional(readOnly = true)
     public MovieDto getMovieById(Long movieId){
-        Movie movie = movieRepository.findById(movieId)
-                .orElseThrow(()->new RuntimeException("Id not found"));
+        Movie movie =findMovieById(movieId);
         return movieMapper.movieToMovieDto(movie);
     }
 
     @Override
     @Transactional
     public MovieDto updateMovie(Long movieId, MovieDto movieDto){
-        Movie movie = movieRepository.findById(movieId)
-                .orElseThrow(()->new RuntimeException("Id not found"));
+        Movie movie = findMovieById(movieId);
         movie.setName(movieDto.getName());
         return movieMapper.movieToMovieDto(movieRepository.save(movie));
     }
@@ -54,12 +54,19 @@ public class MovieServiceImpl implements MovieService {
     @Override
     @Transactional
     public HashMap<String, Object> destroymovie(Long movieId){
-        Movie movie = movieRepository.findById(movieId)
-                .orElseThrow(()->new RuntimeException("Id not found"));
+        Movie movie = findMovieById(movieId);
         HashMap<String, Object> response = new HashMap<>();
         movieRepository.delete(movie);
         response.put("message", "Movie was deleted");
         return response;
+    }
+
+    private Movie findMovieById(Long movieId){
+        Optional<Movie> movie = movieRepository.findById(movieId);
+        if(!movie.isPresent()){
+            throw new ResourceNotFoundException("Movie", "id", movieId);
+        }
+        return movie.get();
     }
 
 }
