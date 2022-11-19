89565736cd16bc2cc29a978ea496256a8d18c2c6
afrivera <andresrfelip@hotmail.com>
2022-11-18 21:23:41 -0500

    add validation to MovieDto fields required

--- src/main/java/org/afrivera/movie/controller/MovieController.java
+++ src/main/java/org/afrivera/movie/controller/MovieController.java
@@ -3,11 +3,11 @@ package org.afrivera.movie.controller;
 import lombok.RequiredArgsConstructor;
 import org.afrivera.movie.dto.MovieDto;
 import org.afrivera.movie.service.MovieService;
-import org.afrivera.movie.service.impl.MovieServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,7 +16,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MovieController {
 
-    private final MovieServiceImpl movieService;
+    private final MovieService movieService;
 
     @GetMapping
     public ResponseEntity<List<MovieDto>> getAllMovies(){
@@ -29,7 +29,7 @@ public class MovieController {
     }
 
     @PostMapping
-    public ResponseEntity<MovieDto> addMovie(@RequestBody MovieDto movieDto){
+    public ResponseEntity<MovieDto> addMovie(@RequestBody @Valid MovieDto movieDto){
         return new ResponseEntity<>(movieService.addMovie(movieDto), HttpStatus.CREATED);
     }
 
--- src/main/java/org/afrivera/movie/dto/MovieDto.java
+++ src/main/java/org/afrivera/movie/dto/MovieDto.java
@@ -2,6 +2,8 @@ package org.afrivera.movie.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 
@@ -15,8 +17,14 @@ import java.util.Date;
 public class MovieDto {
 
     private Long id;
+
+    @NotNull
+    @NotEmpty
     private String name;
     private String image;
+
+    @NotNull
+    @NotEmpty
     private String description;
     private Date release_date;
 }
