502b5f25ecd4310e74f29632b28761f632650f66
afrivera <andresrfelip@hotmail.com>
2022-12-11 17:09:07 -0500

    add jwt dependencies

--- src/main/java/org/afrivera/movie/auth/service/UserService.java
+++ src/main/java/org/afrivera/movie/auth/service/UserService.java
@@ -4,7 +4,6 @@ import org.afrivera.movie.auth.dto.UserRequestDto;
 import org.afrivera.movie.auth.dto.UserResponseDto;
 import org.springframework.transaction.annotation.Transactional;
 
-
 public interface UserService {
     @Transactional
     UserResponseDto addUser(UserRequestDto userRequestDto);
