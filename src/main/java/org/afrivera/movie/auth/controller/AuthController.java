2a0cb8cd69d14614e4fdf97ba3349711590c52cd
afrivera <andresrfelip@hotmail.com>
2022-12-14 16:55:00 -0500

    add validate to LoginDto on AuthController

--- src/main/java/org/afrivera/movie/auth/controller/AuthController.java
+++ src/main/java/org/afrivera/movie/auth/controller/AuthController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -17,7 +19,7 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<?> login(@RequestBody LoginDto loginDto){
+    public ResponseEntity<?> login(@RequestBody @Valid LoginDto loginDto){
         return ResponseEntity.ok(userService.login(loginDto));
     }
 }
